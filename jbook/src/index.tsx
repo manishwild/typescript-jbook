74df8b8f73b47f222a311034e1c3f345fd5721a0
manish <manishwild1000@gmail.com>
2021-04-06 11:55:16 +0200

    yesterday work on es build

---
+++ jbook/src/index.tsx
@@ -0,0 +1,55 @@
+import * as esbuild from 'esbuild-wasm'
+import { useState, useEffect, useRef } from 'react'
+import ReactDom from 'react-dom'
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin'
+
+
+
+const App = () => {
+  const ref = useRef<any>()
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
+
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: '/esbuild.wasm'
+    })
+    //console.log(service)
+  }
+  useEffect(() => {
+   startService()
+  }, []);
+
+  const onclickHandler = async () => {
+    //console.log(input)
+    if (!ref.current) {
+      return
+    }
+    //transform means transpile
+    // const result = await ref.current.transform(input,{
+    //   loader: 'jsx',
+    //   target: 'es2015'
+    // })
+    const result = await ref.current.build({
+      entryPoints: ['index.js'],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin()]
+    })
+    //console.log(result)
+    setCode(result.outputFiles[0].text)
+  }
+
+  return (
+    <div>
+     <textarea value={input} onChange={e => setInput(e.target.value)}></textarea>
+     <div>
+       <button onClick={onclickHandler}>Submit</button>
+     </div>
+     <pre>{code}</pre>
+    </div>
+  )
+}
+
+ReactDom.render(<App />,document.querySelector('#root'))
