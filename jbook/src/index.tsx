b546aec8c44721bd3dc36b2da930e351437ee2ee
manish <manishwild1000@gmail.com>
2021-04-21 19:09:39 +0200

    change

--- jbook/src/index.tsx
+++ jbook/src/index.tsx
@@ -1,64 +1,78 @@
 import * as esbuild from 'esbuild-wasm'
 import { useState, useEffect, useRef } from 'react'
-import ReactDom from 'react-dom'
+import ReactDOM from 'react-dom'
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin'
 import { fetchPlugin } from './plugins/fetch-plugin'
 
-
 const App = () => {
   const ref = useRef<any>()
-  const [input, setInput] = useState('');
-  const [code, setCode] = useState('');
+  const iframe = useRef<any>()
+  const [input, setInput] = useState('')
+  const [code, setCode] = useState('')
 
   const startService = async () => {
     ref.current = await esbuild.startService({
       worker: true,
-      wasmURL: '/esbuild.wasm'
+      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
     })
-    //console.log(service)
   }
   useEffect(() => {
-   startService()
-  }, []);
+    startService()
+  }, [])
 
-  const onclickHandler = async () => {
-    //console.log(input)
+  const onClick = async () => {
     if (!ref.current) {
       return
     }
-    //transform means transpile
-    // const result = await ref.current.transform(input,{
-    //   loader: 'jsx',
-    //   target: 'es2015'
-    // })
+
     const result = await ref.current.build({
       entryPoints: ['index.js'],
       bundle: true,
       write: false,
-      plugins: [
-        unpkgPathPlugin(), 
-        fetchPlugin(input)
-
-      ],
+      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
       define: {
         'process.env.NODE_ENV': '"production"',
         global: 'window',
-        
-      }
+      },
     })
-    //console.log(result)
-    setCode(result.outputFiles[0].text)
+
+    // setCode(result.outputFiles[0].text);
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*')
   }
+  //113
+  const html = `
+    <html>
+      <head></head>
+      <body>
+        <div id="root"></div>
+        <script>
+          window.addEventListener('message', (event) => {
+            try {
+              eval(event.data);
+            } catch (err) {
+              const root = document.querySelector('#root');
+              root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
+              console.error(err);
+            }
+          }, false);
+        </script>
+      </body>
+    </html>
+  `
 
   return (
     <div>
-     <textarea value={input} onChange={e => setInput(e.target.value)}></textarea>
-     <div>
-       <button onClick={onclickHandler}>Submit</button>
-     </div>
-     <pre>{code}</pre>
+      <textarea
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+      <iframe ref={iframe} sandbox="allow-scripts" srcDoc={html} />
     </div>
   )
 }
 
-ReactDom.render(<App />,document.querySelector('#root'))
+ReactDOM.render(<App />, document.querySelector('#root'))
--- jbook/src/plugins/fetch-plugin.ts
+++ jbook/src/plugins/fetch-plugin.ts
@@ -5,28 +5,67 @@ import localForage from 'localforage'
 const fileCache = localForage.createInstance({
   name: 'filecache'
 });
-
+//92
 export const fetchPlugin = (inputCode: string) => {
   return {
     name: 'fetch-plugin',
     setup(build: esbuild.PluginBuild) {
+      build.onLoad({ filter: /(^index\.js$)/ }, () => {
+        return {
+          loader: 'jsx',
+          contents: inputCode,
+        };
+      })
+
+    build.onLoad({ filter: /.*/},async (args: any) => {
+      const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path)
+
+      //if it is,return it immedialtely
+      if (cachedResult) {
+        return cachedResult
+      }
+    })
+
+      build.onLoad({ filter: /.css$/ }, async (args: any) => {
+        
+
+      const {data, request} = await axios.get(args.path)
+
+      const escaped = data.replace(/\n/g, '').replace(/"/g, '\\"').replace(/'/g, "\\'")
+      const contents = `
+       const style = document.createElement('style');
+       style.innerText = '${escaped}';
+       document.head.appendChild(style);
+      `
+
+      const result: esbuild.OnLoadResult =  {
+        loader: 'jsx',
+        contents,
+        resolveDir: new URL('./', request.responseURL).pathname
+      }
+      //store response in cache
+      await fileCache.setItem(args.path, result)
+
+      return result
+      })
+
     build.onLoad({ filter: /.*/ }, async (args: any) => {
       //console.log('onLoad', args);
       
 //this way we can check any version we like  
 //import React,{useState} from 'react@16.0.0'
-      if (args.path === 'index.js') {
-        return {
-          loader: 'jsx',
-          contents: inputCode,
-           // `
-          // import React,{useState} from 'react'
-          //   console.log(React,useState);
-          // `,
-        };
-      } 
+      // if (args.path === 'index.js') {
+      //   return {
+      //     loader: 'jsx',
+      //     contents: inputCode,
+      //      // `
+      //     // import React,{useState} from 'react'
+      //     //   console.log(React,useState);
+      //     // `,
+      //   };
+      // } 
       // check to see if we have already fetched this file
-      // and if it in the cache
+      // and if it in the cache//we already called on new onload thats why we did not have to called individually
       // const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path)
 
       // //if it is,return it immedialtely
@@ -37,21 +76,11 @@ export const fetchPlugin = (inputCode: string) => {
       const {data, request} = await axios.get(args.path)
       //console.log(request)
 
-      //console.log(args.path)
-      const fileType = args.path.match(/.css$/) ? 'css' : 'jsx'
-
-      const escaped = data.replace(/\n/g, '').replace(/"/g, '\\"').replace(/'/g, "\\'")
-      const contents = fileType === 'css' ?
-      `
-       const style = document.createElement('style');
-       style.innerText = '${escaped}';
-       document.head.appendChild(style);
-      ` 
-      : data
+      
 
       const result: esbuild.OnLoadResult =  {
         loader: 'jsx',
-        contents,
+        contents: data,
         resolveDir: new URL('./', request.responseURL).pathname
       }
       //store response in cache
